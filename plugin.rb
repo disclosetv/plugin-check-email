448fa8e8126d4d90e57226c7e11791b390ce2bb9
terrapop <T7Edo4tco41H>
2020-08-27 21:32:40 +0200

    Error in plugin.rb one end missing.

--- plugin.rb
+++ plugin.rb
@@ -12,26 +12,37 @@ enabled_site_setting :plugin_check_email_enabled
 after_initialize do
   module ::DiscoursePluginCheckEmail
 
+    Rails.logger.warn("XXX init")
+
     class EmailValidator < ActiveModel::EachValidator
 
-    def validate_each(record, attribute, value)
-      if email_checker(value)
-        record.errors.add(attribute, "Email is invalid")
-      end
-    end
-
-    def email_checker(email)
-        uri = URI(SiteSetting.plugin_check_email_api_url+email)
-        result = Net::HTTP.get(uri)
-        is_invalid = false
-        if result == "invalid"
-            is_invalid = true
-            else
-            is_invalid = false
+        def validate_each(record, attribute, value)
+            return unless record.should_validate_email_address?
+            if email_checker(value)
+              record.errors.add(attribute, "ERROR INVALID")
+            end
         end
-        return is_invalid
+
+        def email_checker(email)
+            uri = URI(SiteSetting.plugin_check_email_api_url+email)
+            Rails.logger.warn("XXX url: #{uri}")
+            result = Net::HTTP.get(uri)
+            Rails.logger.warn("XXX result: #{result}")
+            is_invalid = false
+            if result == "invalid"
+                is_invalid = true
+                else
+                is_invalid = false
+            end
+            return is_invalid
+        end
     end
 
+    class ::User
+      validate :plugin_check_email
+      def plugin_check_email
+        DiscoursePluginCheckEmail::EmailValidator.new(attributes: :email).validate_each(self, :email, email)
+      end
     end
   end
 end
