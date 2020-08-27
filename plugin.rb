8064b78101efe252d6b1d3786122339d525d8c80
terrapop <T7Edo4tco41H>
2020-08-27 17:39:10 +0200

    First push of plugin. New commit.

--- plugin.rb
+++ plugin.rb
@@ -3,7 +3,7 @@
 # version: 0.0.1
 # authors: Terrapop
 
-# record.errors.add(attribute, I18n.t(:'user.email.not_allowed'))
+# >>> record.errors.add(attribute, I18n.t(:'user.email.not_allowed'))
 
 require 'net/http'
 
