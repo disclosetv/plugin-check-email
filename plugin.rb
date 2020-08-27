4644476cde5f76f9530da2f70f6f2f224537dabd
terrapop <T7Edo4tco41H>
2020-08-27 21:43:40 +0200

    More changed.

--- plugin.rb
+++ plugin.rb
@@ -3,8 +3,6 @@
 # version: 0.0.1
 # authors: terrapop
 
-# record.errors.add(attribute, I18n.t(:'user.email.not_allowed'))
-
 require 'net/http'
 
 enabled_site_setting :plugin_check_email_enabled
@@ -12,22 +10,18 @@ enabled_site_setting :plugin_check_email_enabled
 after_initialize do
   module ::DiscoursePluginCheckEmail
 
-    Rails.logger.warn("XXX init")
-
     class EmailValidator < ActiveModel::EachValidator
 
         def validate_each(record, attribute, value)
             return unless record.should_validate_email_address?
             if email_checker(value)
-              record.errors.add(attribute, "ERROR INVALID")
+              record.errors.add(attribute, "is not allowed from that email provider! Please use another one.")
             end
         end
 
         def email_checker(email)
             uri = URI(SiteSetting.plugin_check_email_api_url+email)
-            Rails.logger.warn("XXX url: #{uri}")
             result = Net::HTTP.get(uri)
-            Rails.logger.warn("XXX result: #{result}")
             is_invalid = false
             if result == "invalid"
                 is_invalid = true
